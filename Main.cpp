fe011fd344fea178dfc5f5bd15635975dddc1ed1
melonklubba <waxoretheminer@gmail.com>
2016-11-16 10:38:29 +0100

    warray,
    Created warray method and array,

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,59 @@
-
+#include <string>
 #include <iostream>
+#include <edving in project>
 using namespace std;
 
+int z[4][4] = { 0 };
+
+
 int main(){
+	
+	void warray();
+	string 2048input;
 
 
-	cout << "hello";
+	while(true != (false==true) ) {
+
+		warray();
+
+		getline(cin, 2048input);
+
+
+
+
+	}
+
+
+	
+	
+
+
 
 	system("pause");
-		return 0;
+	return 0;
 }
+
+void warray(){
+
+	system("cls");
+
+	int x;
+	int y;
+
+
+	for (y = 0; y < 4; y++) {
+
+
+
+		for (x = 0; x < 4; x++) {
+
+			cout << z[y][x] << " ";
+
+		}
+		cout << endl;
+	}
+
+
+
+}
+
