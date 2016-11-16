6b2dcb12d67725318617fe3dabb3e01305d3fb41
melonklubba <waxoretheminer@gmail.com>
2016-11-16 10:41:14 +0100

    Fixed random spaces
    to much spaces, did the fix

--- Main.cpp
+++ Main.cpp
@@ -5,29 +5,17 @@ using namespace std;
 
 int z[4][4] = { 0 };
 
-
 int main(){
 	
 	void warray();
 	string 2048input;
-
 
 	while(true != (false==true) ) {
 
 		warray();
 
 		getline(cin, 2048input);
-
-
-
-
 	}
-
-
-	
-	
-
-
 
 	system("pause");
 	return 0;
@@ -40,20 +28,11 @@ void warray(){
 	int x;
 	int y;
 
-
 	for (y = 0; y < 4; y++) {
-
-
-
 		for (x = 0; x < 4; x++) {
-
 			cout << z[y][x] << " ";
-
-		}
+	}
 		cout << endl;
 	}
-
-
-
-}
+	}
 
